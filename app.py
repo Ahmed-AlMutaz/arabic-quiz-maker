58203d0856a569e9b88727027e33aa26035c1ee8
Ahmed-AlMutaz <2200364@student.eelu.edu.eg>
2026-07-26 22:50:20 +0300

    Debug startup

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from app.main import app as fastapi_app
 print(">>> APP.PY LOADED <<<")
 
 with gr.Blocks(title="Enterprise Arabic Exam SaaS") as demo:
-    gr.Markdown("# Quiz Maker")
-    gr.HTML('<iframe src="/static/index.html" style="width:100%;height:900px;border:none;"></iframe>')
+    gr.HTML(
+        """
+        <iframe src="/static/index.html"
+                style="width:100%;height:950px;border:none;">
+        </iframe>
+        """
+    )
 
 app = gr.mount_gradio_app(fastapi_app, demo, path="/")
-
-if __name__ == "__main__":
-    demo.launch()
